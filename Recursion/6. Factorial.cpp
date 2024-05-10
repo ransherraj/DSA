85cc7ab1eb673e60601b1af703b8282191847114
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2024-05-10 19:03:36 +0530

    Create 6. Factorial.cpp

---
+++ Recursion/6. Factorial.cpp
@@ -0,0 +1,47 @@
+/*
+
+Factorial
+
+
+*****Best Approach****
+Self Explainatory
+
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve3(int n){
+    if(n == 0) return 1;
+    return n * solve3(n-1);
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    int ans = solve3(n);
+    cout<<"Factorial is "<<ans<<endl;
+}
+
+int main(){
+    int t; cin>>t; while(t--)solve();
+}
+
+
+/*
+
+Custom Input :
+2
+5
+6
+
+Output:
+Factorial is 120
+Factorial is 720
+
+
+
+
+*/
