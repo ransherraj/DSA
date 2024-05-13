23f75ec1825a680eeb8dda3fed7239c893e43353
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2024-05-13 16:43:45 +0530

    Create 1. SegmentTreeMaxInRange.cpp

---
+++ SegmentTree/1. SegmentTreeMaxInRange.cpp
@@ -0,0 +1,90 @@
+
+/*
+
+*********Segment tree***********, 
+find sum, maximum in certain range of queries
+in time complexity less than O(n).
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[100005], seg[4*100005];
+
+int query(int ind, int low, int high, int l, int r){
+    //cout<<l<<" "<<r<<endl;
+    if(low >= l && high <= r){
+        return seg[ind];
+    }
+    if(l > high || r < low){              // low     l      r    high
+        return INT_MIN;
+    }
+    
+    int mid = (low + high)/2;
+    int left = query(2*ind + 1, low, mid, l, r);
+    int right = query(2*ind + 2, mid+1, high, l, r);
+    return max(left, right);
+}
+
+void build(int ind, int low, int high){
+    
+    if(low == high){
+        seg[ind] = arr[low];
+        return;
+    }
+    int mid = (low + high)/2;
+    build(2*ind + 1, low, mid);
+    build(2*ind + 2, mid+1, high);
+    seg[ind] = max(seg[2*ind + 1], seg[2*ind + 2]);
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    build(0, 0, n-1);
+    
+    int q;
+    cin>>q;
+    int l, r;
+    for(int i = 0; i<q; i++){
+        cin>>l>>r;
+        cout<<query(0, 0, n-1, l, r)<<endl;
+    }
+}
+
+int main() { 
+    int t; 
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+	return 0;
+}
+
+
+/* 
+
+Input :
+1
+10
+6 4 10 4 2 7 9 2 1 8
+3
+1 2
+1 8
+3 8
+
+Output :
+10
+10
+9
+
+
+
+*/
