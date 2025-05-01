0a956dd66e7f8ece80435dd3bef3685f1a48883a
RANSHER RAJ VERMA <44178727+ransherraj@users.noreply.github.com>
2025-05-01 23:55:24 +0530

    Create GoldbarWithoutRepeatation.cpp

---
+++ DP/Medium/GoldbarWithoutRepeatation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solve(int n, vector<int> &v, int w){
+    if(w == 0) return 1;
+    if(n <= 0 || w < 0) return 0;
+    return solve(n-1, v, w) + solve(n-1, v, w-v[n-1]);
+}
+int main() {
+    int n; cin>>n;
+    vector<int> v(n);
+    for(int i = 0; i<n; i++) cin>>v[i];
+    int w;
+    cin>>w;
+    cout<<solve(n, v, w)<<endl;
+    return 0;
+}
