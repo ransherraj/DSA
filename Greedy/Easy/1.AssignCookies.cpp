c78529b4f1d6248743c8e06a76b6c918302cadea
RANSHER RAJ VERMA <44178727+ransherraj@users.noreply.github.com>
2025-05-03 18:27:19 +0530

    Update 1.AssignCookies.cpp

--- Greedy/Easy/1.AssignCookies.cpp
+++ Greedy/Easy/1.AssignCookies.cpp
@@ -4,3 +4,21 @@
 
 
 **/
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& greed, vector<int>& sizeOfCookies) {
+        int i = 0, j = 0;
+        sort(greed.begin(), greed.end());
+        sort(sizeOfCookies.begin(), sizeOfCookies.end());
+        int n = greed.size();
+        int m = sizeOfCookies.size();
+        while(i < n && j < m){
+            if(greed[i] <= sizeOfCookies[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
